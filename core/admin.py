5c8bbc76e871d8de72c8581fb1304c37ead42369
Naiara Antolin <naiaraantolinperez@gmail.com>
2026-01-15 08:57:47 +0100

    feat: catalogo integrado con diseño y enlaces funcionales

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
 
-# Register your models here.
+
+
+
+
+
+
+from django.contrib import admin
+from .models import Category, Product, ProductImage
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductImage)
