ec89d497c3dcae828a0944e1a2c512649a155414
Naiara Antolin <naiaraantolinperez@gmail.com>
2026-01-28 10:04:51 +0100

    feat: añadidos mapas en cada detalle del producto y añadida sección de mapa de baserris en la home

--- core/models.py
+++ core/models.py
@@ -15,6 +15,9 @@ class Producer(models.Model):
     description = models.TextField(blank=True)
     is_ecological = models.BooleanField(default=False)
 
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+
     def __str__(self):
         return f"{self.name} · {self.municipality}"
 
@@ -37,13 +40,28 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    UNIT_CHOICES = [
+        ('kg', 'kg'),
+        ('g', 'g'),
+        ('unidad', 'unidad'),
+        ('docena', 'docena'),
+        ('pieza', 'pieza'),
+        ('lote', 'lote'),
+        ('botella', 'botella'),
+        ('tarro', 'tarro'),
+    ]
+
+    unit = models.CharField(
+        max_length=20,
+        choices=UNIT_CHOICES,
+        default='kg'
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
         related_name='products'
     )
-    location = models.CharField(max_length=100)
     is_sold = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
